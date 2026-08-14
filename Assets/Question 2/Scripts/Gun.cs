298d95cba569a677c15c8bd114916fd8cb180644
oliverej2302 <oliverejames2302@gmail.com>
2026-08-14 16:33:57 +0100

    fix: projectiles despawn after set time and projectiles handle their own movement and lifespan

--- Assets/Question 2/Scripts/Gun.cs
+++ Assets/Question 2/Scripts/Gun.cs
@@ -6,37 +6,23 @@ using UnityEngine.InputSystem;
 public class Gun : MonoBehaviour
 {
     public Transform WeaponMuzzle;
-    public float DelayBetweenShots = 1f;
+    public float DelayBetweenShotsSeconds = 1f;
     [SerializeField]
-    GameObject _projectile;
-    float _pojectileSpeed = 20f;
+    GameObject _projectilePrefab;
     float _shotTimer = 0;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        MoveProjectiles();
         _shotTimer += Time.deltaTime;
     }
 
     //Get input to fire gun
     public void FireGun(InputAction.CallbackContext context)
     {
-        if (_shotTimer >= DelayBetweenShots)
+        if (_shotTimer >= DelayBetweenShotsSeconds)
         {
             _shotTimer = 0;
-            GameObject spawnProjectile = Instantiate(_projectile, WeaponMuzzle.position, WeaponMuzzle.rotation);
-            spawnProjectile.transform.position += spawnProjectile.transform.forward * _pojectileSpeed * Time.deltaTime;
-        }
-    }
-
-    //move the all active projectiles
-    void MoveProjectiles()
-    {
-        var projectiles = FindObjectsByType<Projectile>(FindObjectsSortMode.None);
-        foreach (Projectile go in projectiles)
-        {
-            go.transform.position += (go.transform.forward * _pojectileSpeed) * Time.deltaTime;
+            Instantiate(_projectilePrefab, WeaponMuzzle.position, WeaponMuzzle.rotation);
         }
     }
 }
--- Assets/Question 2/Scripts/Projectile.cs
+++ Assets/Question 2/Scripts/Projectile.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+    float _speed = 2000f;
+    float _lifetimeSeconds = 10f;
+    float _lifetimeTimer = 0f;
+
+    void Update()
+    {
+        _lifetimeTimer += Time.deltaTime;
+        rb.linearVelocity = transform.forward * _speed * Time.deltaTime;
+
+        if (_lifetimeTimer > _lifetimeSeconds)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     //Hit detection
     private void OnCollisionEnter(Collision collision)
     {
         Targets t = collision.gameObject.GetComponent<Targets>();
         if (t)
+        {
             t.TargetHit();
+        }
+
+        Destroy(gameObject);
     }
 }
